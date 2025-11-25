8c601e15531c45d49f2bc1df193c83caf0eebb36
salmaasle12-jpg <salmaasle12@gmail.com>
2025-11-25 18:17:06 +0200

    listing songs has added!

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-// -- page to navigate
-// page: from HTML CLICK Example /page:/01/demos/index.html
-function loadPage(page) {
+let userObj = 
+{
+    username:"Salma",
+    grade:85,
+    password:"pass123",
+    isConnected:true,
+    address: {
+        country:"isreal",
+        city:"KASH",
+        street:"ben guriuon 10"
 
-// -- Get Ref for HTML ELEMENT BY ITS ID
-// -- contentFrame is iframe element type
-    let iframeElement = document.getElementById("contentFrame");
-
-    // --Give sidebar on mobile
-    iframeElement.src = page;
-
-    // Close sidebar on mobile
-    document.getElementById("sidebar").classList.remove("show");
+    },
+    allgrades:[{csharp:90},80,90,100,85]
 }
+let newGrade = userObj.grade + 10;
+ userObj.grade += 10;
+userObj.id = 1000;
 
-function toggleSidebar() {
-    document.getElementById("sidebar").classList.toggle("show");
+let userObj2 = userObj;
+userObj.grade += 10;
+userObj2.grade = 0;
+let grsde1 = userObj.grade;
+
+
+
+userObj.address.street = "";
+userObj["address"].city = "telaviv";
+
+
+let arr = [userObj, {username:"Salma",
+    grade:85,
+    password:"pass123",
+    isConnected:true,
+    address: {
+        country:"isreal",
+        city:"KASH",
+        street:"ben guriuon 10"
+
+    },
+    allgrades:[{csharp:90},80,90,100,85]
 }
+]
+
+arr[0].allgrades[1] = {cpp:80};
+arr[1].avg = 95;
+let user2 = arr[1];
+user2.oassword = "12345";
